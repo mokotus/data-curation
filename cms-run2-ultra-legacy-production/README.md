fa5969b7c057abb56cc3e717fa7fcdd16adecc5a
Miko Piitsalo <miko.piitsalo@gmail.com>
2020-04-21 16:09:39 +0200

    cms-run2-ultra-legacy-production: add skip option to script and fix readme

--- cms-run2-ultra-legacy-production/README.md
+++ cms-run2-ultra-legacy-production/README.md
@@ -2,19 +2,24 @@
 Repository for extracting provenance information from specified campaign.
 
 ## Setup
-Login to lxplus, choose a folder and clone the branch: `git clone -b run2-ultra-legacy https://github.com/mokotus/data-curation.git`
+1. Login to lxplus and [prepare a CMSSW release area](https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookSetComputerNode#CreateWork)
+2. Under your CMSSW release src-folder, clone the branch: `git clone -b run2-ultra-legacy https://github.com/mokotus/data-curation.git`
 
 ## Usage steps (done every time after login to lxplus)
-1. Go to this folder(cms-run2-ultra-legacy-production) with `cd`
-2. Enable cms environment: `cmsenv`
-3. Generate CERN SSO cookie from credentials. Type `cern-get-sso-cookie --help` for help or see [McM-folder readme](ultra_processing/mcm/README.md#cern-sso-cookie) for examples
-4. Enable voms proxy by typing: `voms-proxy-init --voms cms --rfc --valid 190:00`
-5. Find an empty folder or create one for outputs, for example: `mkdir outputs`
-6. run `run2-ultra-main.py` with proper parameters(see `python2 run2-ultra-main.py --help` for help), example: `python2 run2-ultra-main.py -c RunIIFall15MiniAODv2 ./outputs`
+1. Enter this folder(cms-run2-ultra-legacy-production)
+2. Enable CMS environment: `cmsenv`
+3. Enable voms proxy by typing: `voms-proxy-init --voms cms --rfc --valid 190:00`
+4. Find an empty folder or create one for outputs, for example: `mkdir outputs`
+5. run `run2-ultra-main.py` with campaign name and output folder as parameters(see `python2 run2-ultra-main.py --help` for full info)
+    * Example: `python2 run2-ultra-main.py -c RunIIFall15MiniAODv2 ./outputs`
 
 ### Additional information and links
 * Tested with `Python 2.7.5` on `lxplus.cern.ch`
-* For most actions you will NEED to have a valid CERN SSO cookie
+* Note that certain steps can be skipped using the `--skip [step_number]` flag. If you wish to alter the process more, start from the [main run file](run2-ultra-main.py).
+* For most actions you will NEED to have a valid CERN SSO cookie. The script will try to create the cookie automatically but if it doesn't succeed, you may want to create the cookie manually. There are a few ways to do this (type `cern-get-sso-cookie --help` to see the documentation):
+    * Run [getCookie.sh](ultra_processing/mcm/getCookie.sh)
+    * Or run: `cern-get-sso-cookie -u https://cms-pdmv.cern.ch/mcm/ -o ~/private/prod-cookie.txt --krb`
+    * If all else fails, create cert and key files by following the instructions [here](https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookStartingGrid#ObtainingCert), and then run: `mkdir -p ~/private && cern-get-sso-cookie --url https://cms-pdmv.cern.ch/mcm/ --cert ~/.globus/usercert.pem --key ~/.globus/userkey.pem --o ~/private/prod-cookie.txt`
 * Public APIs do not require a cookie. Index of public API: https://cms-pdmv.cern.ch/mcm/public/restapi/
 * Link to McM: https://cms-pdmv.cern.ch/mcm/
 * McM Rest API: https://cms-pdmv.cern.ch/mcm/restapi
@@ -33,5 +38,5 @@ Login to lxplus, choose a folder and clone the branch: `git clone -b run2-ultra-
 
 ### Todo
 * Making sure the provenance information is in correct format (MiniAOD)
-* Add LHCheader related code to dataset_locator.dump_provenance_lhc
+* Add LHCheader related code to [dump_provenance_lhc](ultra_processing/dataset_locator.py#L39)
 * Nice to have features? (progress, error handling...)
--- cms-run2-ultra-legacy-production/run2-ultra-main.py
+++ cms-run2-ultra-legacy-production/run2-ultra-main.py
@@ -16,56 +16,61 @@ from ultra_processing.dataset_locator import *
 @click.command()
 @click.option('--campaign', '-c', required=True,
               help='Specify campaign name for metadata extraction')
+@click.option('--skip', '-s', required=False, type=click.IntRange(1, 3), multiple=True, help='Skips a step by its number (1, 2 or 3)')
 @click.argument('output_dir_path', type=click.Path(exists=True))
-def main(campaign, output_dir_path):
-    """Downloads from McM and stores all metadata information on DPOA
-    Steps it takes sequentially:
-    1) Downloads for each parent dataset from McM service metadata like: 
-    cmsdriver scripts, configuration files, cross section.
-    2) Stores all this metada information on DPOA storage.
+def main(campaign, skip, output_dir_path):
+    """Downloads from McM and stores all metadata information on DPOA.
+    Steps it takes sequentially:\n
+    1) Downloads configuration files to output_dir_path/configs/\n
+    2) Downloads cmsdriver scripts to output_dir_path/scripts/\n
     3) If root files are available on DAS service, finds location using global
-    locator and runs scripts: edmDumpProv.py and if exists LHC also 
-    edmLHCheader.py
+    locator and runs scripts: edmDumpProv.py and if LHC exists, 
+    edmLHCheader.py. Saves providence dump to output_dir_path/prov/\n
+    If you wish to skip any of these steps, please see the '--skip' option below.
     """
 
-    # 1) === DOWNLOAD DATASETS ===
-
-    # - 1.1) Get metadata of datasets  
+    # Get metadata of datasets in campaign 
     datasets = query_for_key_values(campaign)
 
-    # - 1.2) Get dataset configs
-    print "Getting configs..."
-    config_ids = [ds['config_id'] for ds in datasets]
-    configs = query_for_config_files(config_ids)
+    # === STEP 1) CONFIG FILES ===
+    if 1 in skip:
+        print "(Skipping configs step 1)"
+    else:
+        print "Saving configs..."
+        config_ids = [ds['config_id'] for ds in datasets]
+        configs = query_for_config_files(config_ids)
 
-    # - 1.3) Get dataset cms driver scripts
-    print "Getting cms driver scripts..."
-    prep_ids = [ds['prep_id'] for ds in datasets]
-    scripts = query_for_cms_driver_scripts(prep_ids)
+        # - Save configs to output directory
+        os.mkdir(os.path.join(output_dir_path, "configs"))
+        for i, config_id in enumerate(config_ids):
+            f = open(os.path.join(output_dir_path, "configs", config_id + ""), 'w')
+            f.write(configs[i])
+            f.close()
 
-    # 2) === STORE DATA ===
-    print "Storing data..."
+    # === STEP 2) CMS DRIVER SCRIPTS ===
+    if 2 in skip:
+        print "(Skipping CMS driver scripts step 2)"
+    else:
+        print "Saving CMS driver scripts..."
+        prep_ids = [ds['prep_id'] for ds in datasets]
+        scripts = query_for_cms_driver_scripts(prep_ids)
 
-    # - 2.1) Store configs
-    os.mkdir(os.path.join(output_dir_path, "configs"))
-    for i, config_id in enumerate(config_ids):
-        f = open(os.path.join(output_dir_path, "configs", config_id + ""), 'w')
-        f.write(configs[i])
-        f.close()
+        # - Save cms driver scripts to output directory
+        os.mkdir(os.path.join(output_dir_path, "scripts"))
+        for i, prep_id in enumerate(prep_ids):
+            f = open(os.path.join(output_dir_path, "scripts", prep_id + ".sh"), 'w')
+            f.write(scripts[i])
+            f.close()
 
-    # - 2.2) Store cms driver scripts
-    os.mkdir(os.path.join(output_dir_path, "scripts"))
-    for i, prep_id in enumerate(prep_ids):
-        f = open(os.path.join(output_dir_path, "scripts", prep_id + ".sh"), 'w')
-        f.write(scripts[i])
-        f.close()
-
-    # 3) === DUMP PROV ===
-    print "Dumping provenance..."
-    dump_provenance_lhc(datasets[0]["output_dataset"], output_dir_path) #Testing with one prov file
-
-    # for ds in datasets:
-    #     dump_provenance_lhc(ds["output_dataset"], output_dir_path)
+    # 3) === STEP 3) PROV DUMP ===
+    if 3 in skip:
+        print "(Skipping prov dump step 3)"
+    else:
+        print "Dumping provenance info..."
+        dump_provenance_lhc(datasets[0]["output_dataset"], output_dir_path) #Testing with one prov file
+        # Enable this for all prov files (takes a long time):
+        # for ds in datasets:
+        #     dump_provenance_lhc(ds["output_dataset"], output_dir_path)
 
 if __name__ == '__main__':
     main()
